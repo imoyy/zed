da44f234eac137db19f01c131b7e853c432ddaf0
Moyy <imoyyi@outlook.com>
2025-03-30 19:22:37 +0800

    ...

--- Parse-Rustflags.ps1
+++ Parse-Rustflags.ps1
@@ -7,5 +7,11 @@ if ($rustflags.Length -eq 0) {
 
 $config_path = ".cargo/config.toml"
 $config = Get-Content $config_path | ConvertFrom-Toml
+if (-not $config.target) {
+    $config.target = @{}
+}
+if (-not $config.target.Item("cfg(all())")) {
+    $config.target.Item("cfg(all())") = @{}
+}
 $config.target.Item("cfg(all())").rustflags = $rustflags
 $config | ConvertTo-Toml -Depth 5 | Out-File $config_path
